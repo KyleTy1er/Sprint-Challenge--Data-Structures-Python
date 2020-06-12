709d764afe3e433ef80a8451c40051e4a9a46e78
Kyle Yates <53956594+KyleTy1er@users.noreply.github.com>
2020-06-12 12:39:56 -0700

    changes for approval

--- names/names.py
+++ names/names.py
@@ -5,7 +5,7 @@ start_time = time.time()
 f = open('names_1.txt', 'r')
 names_1 = f.read().split("\n")  # List containing 10000 names
 f.close()
-
+# change
 f = open('names_2.txt', 'r')
 names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
